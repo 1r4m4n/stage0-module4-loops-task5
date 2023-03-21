424f89895caa1e62bf879efc9c2f04e3601a42d6
raman <r4m4n911@gmail.com>
2023-03-21 16:45:13 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -4,7 +4,7 @@ public class Triangle {
     public void printTriangle(int cathetusLength) {
         for (int i = 0; i < cathetusLength; i++) {
             for (int j = 0; j < cathetusLength - i; j++){
-                System.out.println('8');
+                System.out.print('8');
             }
             System.out.println();
         }
