dc5ab919a66cd63cba469c4f5a57d1431629fd4c
raman <r4m4n911@gmail.com>
2023-03-21 16:24:32 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,9 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+            }
+        }
     }
 }
