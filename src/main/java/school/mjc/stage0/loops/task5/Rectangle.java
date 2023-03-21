26f3ca0c711b52f04eda8fed371e7c7f77839375
raman <r4m4n911@gmail.com>
2023-03-21 16:33:50 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -1,7 +1,12 @@
 package school.mjc.stage0.loops.task5;
 
 public class Rectangle {
-    public void printRectangleFrom8s(int length, int height){
-
+    public void printRectangleFrom8s(int length, int height) {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < height; j++) {
+                System.out.println('8');
+            }
+            System.out.println();
+        }
     }
 }
