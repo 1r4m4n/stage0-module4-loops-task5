1e41e7b5255766c14652588270a383b715a5cf09
raman <r4m4n911@gmail.com>
2023-03-21 17:00:23 +0300

    1

--- src/main/java/school/mjc/stage0/loops/task5/Square.java
+++ src/main/java/school/mjc/stage0/loops/task5/Square.java
@@ -9,10 +9,9 @@ public class Square {
             System.out.println();
             System.out.print('8');
             for (int j = 0; j < sideLength - 1; j++) {
-                System.out.println();
+                System.out.print(' ');
             }
             System.out.println('8');
-            System.out.println();
         }
         for (int j = 0; j < sideLength; j++) {
             System.out.print('8');
